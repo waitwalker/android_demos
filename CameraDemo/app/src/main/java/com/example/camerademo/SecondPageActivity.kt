dd46f5fa49cbf6e927a9dc9f55f803d3d2d426cc
waitwalker <waitwalker@163.com>
2020-03-28 21:43:21 +0800

    打开相机demo

---
+++ CameraDemo/app/src/main/java/com/example/camerademo/SecondPageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.camerademo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondPageActivity :AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_layout)
+
+    }
+}
