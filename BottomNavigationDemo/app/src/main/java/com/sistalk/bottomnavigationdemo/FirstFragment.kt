2938d3a053b12a0d3a3ed91a9bc1c0dc1e80a43b
waitwalker <waitwalker@163.com>
2024-04-21 15:24:15 +0800

    属性动画添加

--- BottomNavigationDemo/app/src/main/java/com/sistalk/bottomnavigationdemo/FirstFragment.kt
+++ BottomNavigationDemo/app/src/main/java/com/sistalk/bottomnavigationdemo/FirstFragment.kt
@@ -1,11 +1,14 @@
 package com.sistalk.bottomnavigationdemo
 
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.lifecycle.ViewModelProvider
 
 class FirstFragment : Fragment() {
@@ -14,19 +17,36 @@ class FirstFragment : Fragment() {
         fun newInstance() = FirstFragment()
     }
 
-    private val viewModel: FirstViewModel by viewModels()
-
+    private lateinit var viewModel: FirstViewModel
+    private lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // TODO: Use the ViewModel
     }
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val firstViewModel = activity?.let { ViewModelProvider(it) }?.get(FirstViewModel::class.java)
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        viewModel = activity?.let { ViewModelProvider(it) }?.get(FirstViewModel::class.java)!!
+        val view:View = inflater.inflate(R.layout.fragment_first, container, false)
+        imageView = view.findViewById(R.id.imageView)
+        return view
+    }
+
+    @SuppressLint("Recycle")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val objectAnimator = ObjectAnimator.ofFloat(imageView,"rotation",0F,0F)
+        objectAnimator.duration = 500
+        imageView.setOnClickListener(View.OnClickListener {
+            if (!objectAnimator.isRunning) {
+                objectAnimator.setFloatValues(imageView.rotation,imageView.rotation+100F)
+                objectAnimator.start()
+            }
+
+        })
     }
 }
--- BottomNavigationDemo/app/src/main/java/com/sistalk/bottomnavigationdemo/MainActivity.kt
+++ BottomNavigationDemo/app/src/main/java/com/sistalk/bottomnavigationdemo/MainActivity.kt
@@ -5,16 +5,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navigationController = Navigation.findNavController(this,R.id.fragmentContainerView)
+        val configuration:AppBarConfiguration = AppBarConfiguration.Builder(bottomNavigationView.menu).build()
+        NavigationUI.setupActionBarWithNavController(this,navigationController,configuration)
+        NavigationUI.setupWithNavController(bottomNavigationView,navigationController)
     }
 }
