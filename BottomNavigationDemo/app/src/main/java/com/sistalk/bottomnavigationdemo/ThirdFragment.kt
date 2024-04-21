5f2ec4e7b9e235b929a3a03929c67c5be639cacb
waitwalker <waitwalker@163.com>
2024-04-21 15:41:55 +0800

    移动属性动画

--- BottomNavigationDemo/app/src/main/java/com/sistalk/bottomnavigationdemo/ThirdFragment.kt
+++ BottomNavigationDemo/app/src/main/java/com/sistalk/bottomnavigationdemo/ThirdFragment.kt
@@ -1,11 +1,14 @@
 package com.sistalk.bottomnavigationdemo
 
+import android.animation.ObjectAnimator
 import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import kotlin.random.Random
 
 class ThirdFragment : Fragment() {
 
@@ -14,7 +17,7 @@ class ThirdFragment : Fragment() {
     }
 
     private val viewModel: ThirdViewModel by viewModels()
-
+    lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,6 +28,28 @@ class ThirdFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_third, container, false)
+        val view = inflater.inflate(R.layout.fragment_third, container, false)
+        imageView = view.findViewById(R.id.imageView)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val objectAnimator = ObjectAnimator.ofFloat(imageView,"x",0F)
+        objectAnimator.duration = 500
+        imageView.setOnClickListener(View.OnClickListener {
+            if (!objectAnimator.isRunning) {
+                var value = 0.0f
+                val bool = Random.nextBoolean()
+                value = if (bool) {
+                    100.0f
+                } else {
+                    -100.0f
+                }
+
+                objectAnimator.setFloatValues(imageView.x,imageView.x+value)
+                objectAnimator.start()
+            }
+        })
     }
 }
