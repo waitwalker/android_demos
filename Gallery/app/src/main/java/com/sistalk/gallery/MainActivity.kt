f1a165b558eee9ee83c6c2712e40102bf56da18d
waitwalker <waitwalker@163.com>
2024-04-25 21:43:39 +0800

    视图绑定

--- Gallery/app/src/main/java/com/sistalk/gallery/MainActivity.kt
+++ Gallery/app/src/main/java/com/sistalk/gallery/MainActivity.kt
@@ -5,14 +5,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.sistalk.gallery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        /// 转换为binding
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        /// 这里可以进一步绑定viewmodel
+        /// 设置生命周期
+        binding.lifecycleOwner = this
+//        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -22,10 +32,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        NavigationUI.setupActionBarWithNavController(this,findNavController(R.id.fragmentContainerView))
+        NavigationUI.setupActionBarWithNavController(this,findNavController(binding.fragmentContainerView.id))
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return super.onSupportNavigateUp() || findNavController(R.id.fragmentContainerView).navigateUp()
+        return super.onSupportNavigateUp() || findNavController(binding.fragmentContainerView.id).navigateUp()
     }
 }
