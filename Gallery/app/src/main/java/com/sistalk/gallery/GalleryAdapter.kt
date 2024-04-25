916ff69823b5874185209b261b0c18f4cb203c1b
waitwalker <waitwalker@163.com>
2024-04-25 09:52:01 +0800

    跳转页面处理

--- Gallery/app/src/main/java/com/sistalk/gallery/GalleryAdapter.kt
+++ Gallery/app/src/main/java/com/sistalk/gallery/GalleryAdapter.kt
@@ -1,9 +1,11 @@
 package com.sistalk.gallery
 
 import android.graphics.drawable.Drawable
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -24,7 +26,13 @@ class GalleryAdapter: ListAdapter<PhotoItem, MyViewHolder>(DIFFCALLBACK) {
         val holder = MyViewHolder(view)
 
         holder.itemView.setOnClickListener(View.OnClickListener {
-
+            val photoItem = getItem(holder.adapterPosition)
+            Bundle().apply {
+                putString("fullURL",photoItem.fullURL)
+                putInt("photoId",photoItem.photoId)
+                putString("previewURL",photoItem.previewURL)
+                holder.itemView.findNavController().navigate(R.id.action_galleryFragment_to_photoFragment, this)
+            }
         })
         return holder
     }
