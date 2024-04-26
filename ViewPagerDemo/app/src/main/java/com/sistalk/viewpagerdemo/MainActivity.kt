c480b1d17d5a09f5a437614ba2d1db14e38d7171
waitwalker <waitwalker@163.com>
2024-04-26 09:14:13 +0800

    添加项目

---
+++ ViewPagerDemo/app/src/main/java/com/sistalk/viewpagerdemo/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.sistalk.viewpagerdemo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.sistalk.viewpagerdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        /// 创建adapter
+        binding.viewPager2.adapter = object : FragmentStateAdapter(this) {
+            override fun getItemCount(): Int {
+                return 3
+            }
+
+            override fun createFragment(position: Int): Fragment {
+                return when (position) {
+                    0-> ScaleFragment()
+                    1-> RotateFragment()
+                    else -> TranslateFragment()
+                }
+            }
+
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
