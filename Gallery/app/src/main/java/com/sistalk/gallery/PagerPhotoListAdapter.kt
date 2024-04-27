a221b0a75551ca01d7848cdb8ebf1bb9621357b2
waitwalker <waitwalker@163.com>
2024-04-27 21:26:21 +0800

    ViewPager预览添加

---
+++ Gallery/app/src/main/java/com/sistalk/gallery/PagerPhotoListAdapter.kt
@@ -0,0 +1,41 @@
+package com.sistalk.gallery
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class PagerPhotoListAdapter:ListAdapter<PhotoItem,PagerPhotoViewHolder>(DiffCallback) {
+    // 需要实现比较器
+    object DiffCallback:DiffUtil.ItemCallback<PhotoItem>(){
+        override fun areItemsTheSame(oldItem: PhotoItem, newItem: PhotoItem): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: PhotoItem, newItem: PhotoItem): Boolean {
+            return oldItem.photoId == newItem.photoId
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PagerPhotoViewHolder {
+        LayoutInflater.from(parent.context).inflate(R.layout.pager_photo_view,parent,false).apply {
+            return PagerPhotoViewHolder(this)
+        }
+    }
+
+    override fun onBindViewHolder(holder: PagerPhotoViewHolder, position: Int) {
+        Glide.with(holder.itemView)
+            .load(getItem(position))
+            .placeholder(R.drawable.baseline_all_inbox_24)
+            .into(holder.itemView.findViewById(R.id.pagerPhoto))
+    }
+
+}
+
+class PagerPhotoViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+
+}
