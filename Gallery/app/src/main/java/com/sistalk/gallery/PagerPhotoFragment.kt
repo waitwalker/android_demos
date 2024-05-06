ab085561d660a9e4228379cad421683f822c59c1
waitwalker <waitwalker@163.com>
2024-05-06 09:17:40 +0800

    保存按钮添加

--- Gallery/app/src/main/java/com/sistalk/gallery/PagerPhotoFragment.kt
+++ Gallery/app/src/main/java/com/sistalk/gallery/PagerPhotoFragment.kt
@@ -1,11 +1,15 @@
 package com.sistalk.gallery
 
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.viewpager2.widget.ViewPager2
 import com.sistalk.gallery.databinding.FragmentPagerPhotoBinding
@@ -40,7 +44,7 @@ class PagerPhotoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_pager_photo,container,false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_pager_photo, container, false)
         return binding.root
         // Inflate the layout for this fragment
         // return inflater.inflate(R.layout.fragment_pager_photo, container, false)
@@ -51,17 +55,17 @@ class PagerPhotoFragment : Fragment() {
         val photoList: ArrayList<String>? = arguments?.getStringArrayList("photos")
         PagerPhotoListAdapter().apply {
             binding.viewPager2.adapter = this
-            val list:ArrayList<PhotoItem> = ArrayList()
+            val list: ArrayList<PhotoItem> = ArrayList()
             var index = 0;
             photoList?.map {
-                val photoItem = PhotoItem(it,index,it,0,"zhang",100,100)
+                val photoItem = PhotoItem(it, index, it, 0, "zhang", 100, 100)
                 list.add(photoItem)
                 index++
             }
             submitList(list)
         }
 
-        binding.viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+        binding.viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageScrolled(
                 position: Int,
                 positionOffset: Float,
@@ -82,7 +86,13 @@ class PagerPhotoFragment : Fragment() {
             }
         })
 
-        binding.viewPager2.setCurrentItem(arguments?.getInt("currentIndex")?:0, false)
+        binding.viewPager2.setCurrentItem(arguments?.getInt("currentIndex") ?: 0, false)
+
+        binding.imageViewSave.setOnClickListener {
+            if (Build.VERSION.SDK_INT < 29) {
+                Log.d("photo view","");
+            }
+        }
     }
 
     companion object {
