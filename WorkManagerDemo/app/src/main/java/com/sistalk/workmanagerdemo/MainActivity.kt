790bd8b1ddc65cad66d4a2d57c6489e07b6b400b
waitwalker <waitwalker@163.com>
2024-05-14 08:41:49 +0800

    worker 使用

--- WorkManagerDemo/app/src/main/java/com/sistalk/workmanagerdemo/MainActivity.kt
+++ WorkManagerDemo/app/src/main/java/com/sistalk/workmanagerdemo/MainActivity.kt
@@ -5,12 +5,32 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import com.sistalk.workmanagerdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding:ActivityMainBinding
+
+    // 创建worker 工作者
+    private val workerManager = WorkManager.getInstance(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+        // setContentView(R.layout.activity_main)
+        mainBinding.button.setOnClickListener {
+            // 工作请求，创建工作（可以是单次的或者周期性的）
+            val workRequest:OneTimeWorkRequest = OneTimeWorkRequestBuilder<MyWorker>()
+                .build()
+            workerManager.enqueue(workRequest)
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- WorkManagerDemo/app/src/main/java/com/sistalk/workmanagerdemo/MyWorker.kt
+++ WorkManagerDemo/app/src/main/java/com/sistalk/workmanagerdemo/MyWorker.kt
@@ -1,4 +1,18 @@
 package com.sistalk.workmanagerdemo
 
-class MyWorker {
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class MyWorker(context: Context, workerParams: WorkerParameters) :Worker(context, workerParams) {
+    private val tag = "MyWorker"
+    override fun doWork(): Result {
+        Log.d(tag,"doWork:started")
+        Thread.sleep(3000)
+        Log.d(tag,"doWork:finished")
+        return Result.success()
+    }
+
+
 }
