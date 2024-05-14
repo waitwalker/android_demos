a2fb04de8b462efaef16b4c1bd72e7ab07e09b57
waitwalker <waitwalker@163.com>
2024-05-14 09:12:05 +0800

    添加级联工作

--- WorkManagerDemo/app/src/main/java/com/sistalk/workmanagerdemo/MainActivity.kt
+++ WorkManagerDemo/app/src/main/java/com/sistalk/workmanagerdemo/MainActivity.kt
@@ -6,24 +6,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.Observer
 import androidx.work.Constraints
 import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkInfo
 import androidx.work.WorkManager
-import androidx.work.setInputMerger
 import androidx.work.workDataOf
 import com.sistalk.workmanagerdemo.databinding.ActivityMainBinding
 
 const val INPUT_DATA_KEY = "input_data_key"
 const val OUTPUT_DATA_KEY = "output_data_key"
 const val WORK_A_NAME = "WorkA"
+const val WORK_B_NAME = "WorkB"
+
 class MainActivity : AppCompatActivity() {
     private val tag = "MainActivity"
 
-    private lateinit var mainBinding:ActivityMainBinding
+    private lateinit var mainBinding: ActivityMainBinding
 
     // 创建worker 工作者
     private val workerManager = WorkManager.getInstance(this)
@@ -34,23 +34,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(mainBinding.root)
         // setContentView(R.layout.activity_main)
         mainBinding.button.setOnClickListener {
-            // 约束条件
-            val constraints = Constraints.Builder()
-                .setRequiredNetworkType(NetworkType.CONNECTED)
-                .build()
-            // 工作请求，创建工作（可以是单次的或者周期性的）
-            val workRequest:OneTimeWorkRequest = OneTimeWorkRequestBuilder<MyWorker>()
-                .setConstraints(constraints)
-                .setInputData(workDataOf(INPUT_DATA_KEY to WORK_A_NAME))
-                .build()
-            workerManager.enqueue(workRequest)
-
-            workerManager.getWorkInfoByIdLiveData(workRequest.id).observe(this, Observer {
-                Log.d(tag,"state:${it?.state}")
-                if (it?.state == WorkInfo.State.SUCCEEDED) {
-                    Log.d(tag,"data:${it.outputData.getString(OUTPUT_DATA_KEY)}")
-                }
-            })
+            val workRequestA: OneTimeWorkRequest = oneTimeWorkRequest(WORK_A_NAME)
+            val workRequestB: OneTimeWorkRequest = oneTimeWorkRequest(WORK_B_NAME)
+            workerManager.beginWith(workRequestA)
+                .then(workRequestB)
+                .enqueue()
+//            workerManager.enqueue(workRequestA)
+//
+//            // 监听工作状态
+//            workerManager.getWorkInfoByIdLiveData(workRequestA.id).observe(this) {
+//                Log.d(tag, "state:${it?.state}")
+//                if (it?.state == WorkInfo.State.SUCCEEDED) {
+//                    Log.d(tag, "data:${it.outputData.getString(OUTPUT_DATA_KEY)}")
+//                }
+//            }
         }
 
 
@@ -60,5 +57,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun oneTimeWorkRequest(name: String): OneTimeWorkRequest {
+        // 约束条件
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+        // 工作请求，创建工作（可以是单次的或者周期性的）
+        val workRequest: OneTimeWorkRequest = OneTimeWorkRequestBuilder<MyWorker>()
+            .setConstraints(constraints)
+            .setInputData(workDataOf(INPUT_DATA_KEY to name))
+            .build()
+        return workRequest
     }
 }
