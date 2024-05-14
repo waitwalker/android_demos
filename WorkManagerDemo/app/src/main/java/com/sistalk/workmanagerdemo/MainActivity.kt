a1bff6f91eb1f3a819b31ec2f44b33b2841d37d8
waitwalker <waitwalker@163.com>
2024-05-14 09:00:24 +0800

    监听工作的状态

--- WorkManagerDemo/app/src/main/java/com/sistalk/workmanagerdemo/MainActivity.kt
+++ WorkManagerDemo/app/src/main/java/com/sistalk/workmanagerdemo/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.sistalk.workmanagerdemo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
 import androidx.work.Constraints
 import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkInfo
 import androidx.work.WorkManager
+import androidx.work.setInputMerger
+import androidx.work.workDataOf
 import com.sistalk.workmanagerdemo.databinding.ActivityMainBinding
 
+const val INPUT_DATA_KEY = "input_data_key"
+const val OUTPUT_DATA_KEY = "output_data_key"
+const val WORK_A_NAME = "WorkA"
 class MainActivity : AppCompatActivity() {
+    private val tag = "MainActivity"
 
     private lateinit var mainBinding:ActivityMainBinding
 
@@ -32,8 +41,16 @@ class MainActivity : AppCompatActivity() {
             // 工作请求，创建工作（可以是单次的或者周期性的）
             val workRequest:OneTimeWorkRequest = OneTimeWorkRequestBuilder<MyWorker>()
                 .setConstraints(constraints)
+                .setInputData(workDataOf(INPUT_DATA_KEY to WORK_A_NAME))
                 .build()
             workerManager.enqueue(workRequest)
+
+            workerManager.getWorkInfoByIdLiveData(workRequest.id).observe(this, Observer {
+                Log.d(tag,"state:${it?.state}")
+                if (it?.state == WorkInfo.State.SUCCEEDED) {
+                    Log.d(tag,"data:${it.outputData.getString(OUTPUT_DATA_KEY)}")
+                }
+            })
         }
 
 
--- WorkManagerDemo/app/src/main/java/com/sistalk/workmanagerdemo/MyWorker.kt
+++ WorkManagerDemo/app/src/main/java/com/sistalk/workmanagerdemo/MyWorker.kt
@@ -4,15 +4,15 @@ import android.content.Context
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import androidx.work.workDataOf
 
 class MyWorker(context: Context, workerParams: WorkerParameters) :Worker(context, workerParams) {
     private val tag = "MyWorker"
     override fun doWork(): Result {
-        Log.d(tag,"doWork:started")
+        val name = inputData.getString(INPUT_DATA_KEY)
+        Log.d(tag,"doWork:$name started")
         Thread.sleep(3000)
-        Log.d(tag,"doWork:finished")
-        return Result.success()
+        Log.d(tag,"doWork:$name finished")
+        return Result.success(workDataOf(OUTPUT_DATA_KEY to "$name output"))
     }
-
-
 }
