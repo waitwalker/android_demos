df2456bfda4f62d03b5aa4f9cde8519192641949
waitwalker <waitwalker@163.com>
2024-05-18 15:50:46 +0800

    添加抽屉页面容器

--- NavgationDrawerDemo/app/src/main/java/com/sistalk/navgationdrawerdemo/MainActivity.kt
+++ NavgationDrawerDemo/app/src/main/java/com/sistalk/navgationdrawerdemo/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 //        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawerLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
