cbf6d851f6ad0a702154b7c5612e5cfa688dc5f0
waitwalker <waitwalker@163.com>
2024-05-18 16:38:16 +0800

    抽屉功能基本实现

--- NavgationDrawerDemo/app/src/main/java/com/sistalk/navgationdrawerdemo/MainActivity.kt
+++ NavgationDrawerDemo/app/src/main/java/com/sistalk/navgationdrawerdemo/MainActivity.kt
@@ -4,21 +4,46 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.sistalk.navgationdrawerdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var navController: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 //        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawerLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        navController = findNavController(binding.fragment.id)
+//        appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
+        val set:Set<Int> = setOf(R.id.textFragment,R.id.listFragment,R.id.pagerFragment)
+        appBarConfiguration = AppBarConfiguration(set,binding.drawerLayout)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        binding.navigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration = appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
