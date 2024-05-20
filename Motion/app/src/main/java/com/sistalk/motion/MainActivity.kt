aa81b645a95356860f8bae0b5d3af5c57d84f137
waitwalker <waitwalker@163.com>
2024-05-20 08:35:11 +0800

    通过代码控制动画

--- Motion/app/src/main/java/com/sistalk/motion/MainActivity.kt
+++ Motion/app/src/main/java/com/sistalk/motion/MainActivity.kt
@@ -5,12 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.sistalk.motion.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+        val set = mainBinding.motionLayout.getConstraintSet(R.id.end)
+        set.setRotation(R.id.view,9000f)
+        mainBinding.motionLayout.updateState(R.id.end,set)
+        // setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.motionLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
