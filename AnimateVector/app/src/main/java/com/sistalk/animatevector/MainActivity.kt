2cce8d4bfe08f3bc66b6f09177017b11bc605791
waitwalker <waitwalker@163.com>
2024-05-23 07:28:57 +0800

    lottie结合Motion实现

--- AnimateVector/app/src/main/java/com/sistalk/animatevector/MainActivity.kt
+++ AnimateVector/app/src/main/java/com/sistalk/animatevector/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sistalk.animatevector
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.sistalk.animatevector.databinding.ActivityMainBinding
@@ -17,6 +18,39 @@ class MainActivity : AppCompatActivity() {
         mainBinding.lottieView.setOnClickListener {
             mainBinding.lottieView.playAnimation()
         }
+
+        mainBinding.main.setTransitionListener(object : MotionLayout.TransitionListener{
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+            }
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+                mainBinding.lottieView.progress = progress
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+
+            }
+
+        })
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
