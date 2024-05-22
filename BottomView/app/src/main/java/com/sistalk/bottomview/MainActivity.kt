ce4578b8648166154c24df27fa39cc0aa831887f
waitwalker <waitwalker@163.com>
2024-05-22 09:19:15 +0800

    点击页面切换处理

--- BottomView/app/src/main/java/com/sistalk/bottomview/MainActivity.kt
+++ BottomView/app/src/main/java/com/sistalk/bottomview/MainActivity.kt
@@ -3,13 +3,17 @@ package com.sistalk.bottomview
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.sistalk.bottomview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainBinding: ActivityMainBinding
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,6 +24,23 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        navController = findNavController(R.id.fragmentContainerView)
+        findViewById<MotionLayout>(R.id.messageMotionLayout).setOnClickListener{
+            navController.navigate(R.id.messageFragment)
+        }
+        findViewById<MotionLayout>(R.id.contactMotionLayout).setOnClickListener{
+            navController.navigate(R.id.contactFragment)
+        }
+        findViewById<MotionLayout>(R.id.exploreMotionLayout).setOnClickListener{
+            navController.navigate(R.id.exploreFragment)
+        }
+        findViewById<MotionLayout>(R.id.accountMotionLayout).setOnClickListener{
+            navController.navigate(R.id.exploreFragment)
         }
     }
 }
