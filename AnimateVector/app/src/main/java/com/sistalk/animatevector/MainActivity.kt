c5bfbdf9888f0be5361de58b7f4bbfbde42a4e17
waitwalker <waitwalker@163.com>
2024-05-23 07:22:03 +0800

    lottie重拾

---
+++ AnimateVector/app/src/main/java/com/sistalk/animatevector/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.sistalk.animatevector
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.sistalk.animatevector.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+        mainBinding.lottieView.setOnClickListener {
+            mainBinding.lottieView.playAnimation()
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
