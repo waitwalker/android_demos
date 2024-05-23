d9de3d7236748e4daeadb8876bf9cd8bfe736732
waitwalker <waitwalker@163.com>
2024-05-23 08:18:34 +0800

    Media媒体重拾

---
+++ MediaPlayerDemo/app/src/main/java/com/sistalk/mediaplayerdemo/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.sistalk.mediaplayerdemo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.sistalk.mediaplayerdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        val videoPath = "android.resource://$packageName/${R.raw.a1}"
+        mainBinding.videoView.setVideoPath(videoPath)
+        mainBinding.videoView.start()
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
