6673e2d00d58644c17d56be4ace57927e78fa5fd
waitwalker <waitwalker@163.com>
2024-05-24 09:24:18 +0800

    处理监听

--- MyPlayer/app/src/main/java/com/sistalk/myplayer/MainActivity.kt
+++ MyPlayer/app/src/main/java/com/sistalk/myplayer/MainActivity.kt
@@ -5,12 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.sistalk.myplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var playerViewModel: PlayerViewModel? = null
+    private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        playerViewModel = mainBinding.data
+        mainBinding.lifecycleOwner = this
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- MyPlayer/app/src/main/java/com/sistalk/myplayer/PlayerViewModel.kt
+++ MyPlayer/app/src/main/java/com/sistalk/myplayer/PlayerViewModel.kt
@@ -1,4 +1,40 @@
 package com.sistalk.myplayer
 
-class PlayerViewModel {
+import android.media.MediaPlayer
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class PlayerViewModel:ViewModel() {
+    private val mediaPlayer = MediaPlayer()
+
+    private val _progressBarVisibility = MutableLiveData(View.VISIBLE)
+    val progressBarVisibility:LiveData<Int> = _progressBarVisibility
+    private val _videoResolution = MutableLiveData(Pair(0,0))
+    val videoResolution:LiveData<Pair<Int,Int>> = _videoResolution
+
+    fun loadVideo() {
+        mediaPlayer.apply {
+            _progressBarVisibility.value = View.VISIBLE
+            setDataSource("https://www.youtube.com/watch?v=rDvxlZcpcI4")
+            setOnPreparedListener{
+                _progressBarVisibility.value = View.INVISIBLE
+                isLooping = true
+                it.start()
+            }
+
+            setOnVideoSizeChangedListener { _, width, height ->
+                _videoResolution.value = Pair(width,height)
+            }
+            prepareAsync()
+        }
+    }
+
+    // 释放资源
+    override fun onCleared() {
+        super.onCleared()
+        // 释放占用的软硬件资源主要硬件资源
+        mediaPlayer.release()
+    }
 }
