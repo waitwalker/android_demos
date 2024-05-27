b94fd3e0711638316502872c1cbfe6d75d350a32
waitwalker <waitwalker@163.com>
2024-05-27 09:10:36 +0800

    服务存活验证

--- ServiceDemo/app/src/main/java/com/sistalk/servicedemo/SecondActivity.kt
+++ ServiceDemo/app/src/main/java/com/sistalk/servicedemo/SecondActivity.kt
@@ -6,39 +6,30 @@ import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.IBinder
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.Observer
-import com.sistalk.servicedemo.databinding.ActivityMainBinding
+import com.sistalk.servicedemo.databinding.ActivitySecondBinding
 
-class MainActivity : AppCompatActivity() {
-    private val tag = "Hello"
+class SecondActivity : AppCompatActivity() {
 
-    private lateinit var viewBinding: ActivityMainBinding
+    private lateinit var viewActivitySecondBinding: ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.i(tag, "onCreate:MainActivity")
         enableEdgeToEdge()
-//        setContentView(R.layout.activity_main)
-        viewBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(viewBinding.main)
+
+        viewActivitySecondBinding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(viewActivitySecondBinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        viewBinding.button.setOnClickListener {
-            Intent(this, MyService::class.java).also {
-                //intent还可以放一些参数
-                startService(it)
-            }
-        }
 
-        viewBinding.bindService.setOnClickListener {
+        viewActivitySecondBinding.startBind.setOnClickListener {
+            // 测试服务一直存在
             val bindIntent = Intent(this, MyService::class.java)
             val serviceConnection = object : ServiceConnection {
                 // 取消绑定
@@ -50,42 +41,15 @@ class MainActivity : AppCompatActivity() {
                 override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
                     //  name服务名称 service可供访问的服务
                     (service as MyService.MyBinder).service.numberLiveData.observe(
-                        this@MainActivity
+                        this@SecondActivity
                     ) {
-                        viewBinding.textView.text = "$it"
+                        viewActivitySecondBinding.textView2.text = "$it"
                     }
                 }
 
 
             }
-            startService(bindIntent)
             bindService(bindIntent, serviceConnection, Context.BIND_AUTO_CREATE)
         }
-
-        viewBinding.startSecondActivity.setOnClickListener {
-            Intent(this,SecondActivity::class.java).also {
-                startActivity(it)
-            }
-        }
     }
-
-    override fun onStart() {
-        super.onStart()
-        Log.i(tag, "onStart:MainActivity")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.i(tag, "onStop:MainActivity")
-//        Intent(this,MyService::class.java).also {
-//            stopService(it)
-//        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i(tag, "onDestroy:MainActivity")
-    }
-
-
 }
