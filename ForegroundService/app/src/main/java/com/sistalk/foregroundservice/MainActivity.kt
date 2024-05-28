4b958bc35034504a3d08f545914ed30636825c08
waitwalker <waitwalker@163.com>
2024-05-28 08:02:24 +0800

    创建前台服务

---
+++ ForegroundService/app/src/main/java/com/sistalk/foregroundservice/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.sistalk.foregroundservice
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.sistalk.foregroundservice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // 启动Service
+        Intent(this,MyService::class.java).apply {
+            startService(this)
+        }
+    }
+}
---
+++ ForegroundService/app/src/main/java/com/sistalk/foregroundservice/MyService.kt
@@ -0,0 +1,60 @@
+package com.sistalk.foregroundservice
+
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+const val CHANNEL_ID = "my notification channel ID"
+
+class MyService : LifecycleService() {
+
+    private var number = 0
+
+    @SuppressLint("ForegroundServiceType")
+    override fun onCreate() {
+        super.onCreate()
+        lifecycleScope.launch {
+            while (true) {
+                delay(1000)
+                Log.d("Hello","onCreate:${number++}")
+            }
+        }
+        createChannel()
+
+        // 8.0以后必须创建channel，并且channel是用户可以在设置中可以看见&关闭的
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_android_black_24dp)
+            .setContentTitle("This start")
+            .setContentTitle("content")
+            .build()
+        // 启动前台服务
+        startForeground(1,notification)
+    }
+    private fun createChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "KaiSi"
+            val desc = "通知desc"
+            // 通知级别
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val mChannel = NotificationChannel(CHANNEL_ID,name,importance)
+            mChannel.description = desc
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(mChannel)
+
+        }
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        super.onBind(intent)
+        TODO("Return the communication channel to the service.")
+    }
+}
