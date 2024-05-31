730f6c402f53c5eb36a27af29ff5ee229f3b6c3f
waitwalker <waitwalker@163.com>
2024-05-31 09:18:35 +0800

    发送广播

---
+++ SendBroadcastDemo/app/src/main/java/com/sistalk/sendbroadcastdemo/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.sistalk.sendbroadcastdemo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.sistalk.sendbroadcastdemo.databinding.ActivityMainBinding
+import android.Manifest
+import android.content.Intent
+const val BROADCAST_ACTION = "com."
+class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+        //setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        mainBinding.sendButton.setOnClickListener {
+            Intent().apply {
+                setAction("")
+            }
+        }
+    }
+}
