51dd5701c2b4ce643d8e1ce7fd0aee0523b6153e
waitwalker <waitwalker@163.com>
2024-06-04 08:04:34 +0800

    通过PackageManager查询已安装状态跳转（适用Android10.0以下系统）

--- AppA/app/src/main/java/com/sistalk/appa/MainActivity.kt
+++ AppA/app/src/main/java/com/sistalk/appa/MainActivity.kt
@@ -3,8 +3,10 @@ package com.sistalk.appa
 import android.annotation.SuppressLint
 import android.content.ComponentName
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -28,6 +30,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        // 隐式intent跳转
         mainBinding.jumpButton.setOnClickListener {
             // 隐式跳转
             val mIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://waitwalker.cn"))
@@ -38,6 +41,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // 显示intent跳转
         mainBinding.button2.setOnClickListener {
             val mIntent = Intent()
             mIntent.component = ComponentName("com.sistalk.appb","com.sistalk.appb.SecondActivity")
@@ -47,5 +51,36 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "App B is not installed.", Toast.LENGTH_LONG).show();
             }
         }
+
+        // 通过package manager 方式已经过期，涉及到隐私权限
+        mainBinding.buttonPackage.setOnClickListener {
+            val packageName = "com.sistalk.appc"
+            if (isAppInstalled(packageName)) {
+                val mIntent = packageManager.getLaunchIntentForPackage(packageName)
+                if (mIntent != null) {
+                    mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(mIntent)
+                }
+            } else {
+                Toast.makeText(this,"$packageName 没有安装",Toast.LENGTH_LONG).show()
+            }
+
+
+        }
+    }
+
+    private fun isAppInstalled(packageName:String): Boolean{
+        var installed = false
+        try {
+            val packageInfo = packageManager.getPackageInfo(packageName,PackageManager.GET_ACTIVITIES)
+            if (packageInfo.packageName == packageName) {
+                installed = true
+            }
+        } catch (e:Exception) {
+            installed = false
+        } finally {
+            Log.d("MainActivity","应用安装状态:$installed")
+        }
+        return installed
     }
 }
