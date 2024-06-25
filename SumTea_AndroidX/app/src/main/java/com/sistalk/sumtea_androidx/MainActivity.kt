530d6fd842c94407432efc45b84fc5b6a6620ac5
waitwalker <waitwalker@163.com>
2024-06-25 08:53:51 +0800

    线程间通信处理

--- SumTea_AndroidX/app/src/main/java/com/sistalk/sumtea_androidx/MainActivity.kt
+++ SumTea_AndroidX/app/src/main/java/com/sistalk/sumtea_androidx/MainActivity.kt
@@ -1,18 +1,56 @@
 package com.sistalk.sumtea_androidx
-
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.sistalk.framework.log.LogUtil
 import com.sistalk.sumtea_androidx.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var mainBinding: ActivityMainBinding
+    private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var mainHandler: Handler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
+        handleMainMessage()
+
+        mainBinding.mainSend.setOnClickListener {
+            sendToMain()
+        }
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun handleMainMessage() {
+        mainHandler = Handler(mainLooper) {
+            LogUtil.d("获取的消息：${it.obj}")
+            mainBinding.textView.text = "${it.obj}"
+            false
+        }
+    }
+
+    private fun sendToMain() {
+        Thread(kotlinx.coroutines.Runnable {
+            run {
+                val result = performComplexCalculation()
+                val message = mainHandler.obtainMessage(1,result)
+                mainHandler.sendMessage(message)
+            }
+        }).start()
+    }
+
+    private fun performComplexCalculation():Int {
+        try {
+            Thread.sleep(2000)
+        } catch (e:Exception) {
+            e.printStackTrace()
+        }
+        return 42
     }
 }
