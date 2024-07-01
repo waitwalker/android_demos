dfa6287458ce09f16f0a960c03b34ac0f4e2e6a2
waitwalker <waitwalker@163.com>
2024-07-01 10:14:03 +0800

    Volatile关键字处理

--- SumTea_AndroidX/lib_starter/src/main/java/com/sistalk/starter/task/Task.kt
+++ SumTea_AndroidX/lib_starter/src/main/java/com/sistalk/starter/task/Task.kt
@@ -1,7 +1,17 @@
 package com.sistalk.starter.task
 
+import android.content.Context
+import com.sistalk.starter.dispatcher.TaskDispatcher
+
 /**
  * Task基类
  * */
 abstract class Task : ITask{
+
+    protected var mContext:Context? = context
+
+    protected var mIsMainProcess:Boolean = TaskDispatcher.isMainProcess
+
+    @Volatile
+    var isWaiting = false
 }
