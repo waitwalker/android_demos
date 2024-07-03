823d7eb52128f7b899a291cf5c71f718fe2df73c
waitwalker <waitwalker@163.com>
2024-07-03 08:43:13 +0800

    任务基类编写处理

--- SumTea_AndroidX/lib_starter/src/main/java/com/sistalk/starter/task/Task.kt
+++ SumTea_AndroidX/lib_starter/src/main/java/com/sistalk/starter/task/Task.kt
@@ -1,12 +1,12 @@
 package com.sistalk.starter.task
-
 import android.content.Context
 import android.os.Process
 import com.sistalk.framework.log.LogUtil
 import com.sistalk.starter.dispatcher.TaskDispatcher
 import com.sistalk.starter.dispatcher.TaskDispatcher.Companion.context
+import com.sistalk.starter.utils.DispatcherExecutor
 import java.util.concurrent.CountDownLatch
-import java.util.concurrent.Executor
+import java.util.concurrent.ExecutorService
 
 /**
  * Task基类
@@ -78,9 +78,55 @@ abstract class Task : ITask{
      * Task执行在哪个线程池，默认在IO的线程池
      * CPU密集型的一定要切换到DispatcherExecutor.getCPUExecutor()
      * */
-    override fun runOn(): Executor? {
+    override fun runOn(): ExecutorService? {
+        return DispatcherExecutor.IOExecutor
+    }
+
+    /**
+     * 异步线程执行的Task是否需要在被调用await的时候等待，默认不需要
+     * */
+    override fun needWait(): Boolean {
+        return false
+    }
+
+    /**
+     * 当前Task依赖的Task集合（需要等待被依赖的Task执行完毕才能执行自己），默认没有依赖
+     * */
+    override fun dependsOn(): List<Class<out Task?>?>? {
+        return null
+    }
+
+    /**
+     * Task是否运行在主线程，默认false
+     * */
+    override fun runOnMainThread(): Boolean {
+        return false
+    }
+
+    override val tailRunnable: Runnable?
+        get() = null
+
+    override fun setTaskCallBack(callBack: TaskCallBack?) {
 
     }
 
+    override fun needCall(): Boolean {
+        return false
+    }
 
+    /**
+     * 是否只在主进程，默认是
+     * */
+    override fun onlyInMainProcess(): Boolean {
+        return true
+    }
+}
+
+/**
+ * 主线程任务
+ * */
+abstract class MainTask:Task() {
+    override fun runOnMainThread(): Boolean {
+        return true
+    }
 }
