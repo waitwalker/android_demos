e45c35be21100a28dca7ce4481d891ac61880032
waitwalker <waitwalker@163.com>
2024-07-06 13:06:43 +0800

    添加APP配置相关&获取设备信息

--- SumTea_AndroidX/lib_framework/src/main/java/com/sistalk/framework/manager/AppManager.kt
+++ SumTea_AndroidX/lib_framework/src/main/java/com/sistalk/framework/manager/AppManager.kt
@@ -2,16 +2,17 @@ package com.sistalk.framework.manager
 
 import android.annotation.SuppressLint
 import android.app.Application
-import android.content.Context
-import android.hardware.display.DisplayManager
-import android.view.Display
+import android.content.res.Resources
+import android.provider.Settings
+import com.sistalk.framework.log.LogUtil
+import com.sistalk.framework.utils.DeviceInfoUtils
 import kotlin.math.max
 
 
 object AppManager {
 
     private val tag = AppManager::class.java.simpleName
-    private lateinit var mContext:Application
+    private lateinit var mContext: Application
 
     private var mScreenWidthPx = 0
     private var mScreenHeightPx = 0
@@ -20,20 +21,19 @@ object AppManager {
 
     // 像素密度 每英寸像素个数
     private var mDensityDpi = 0
+
     // density scale
     private var mDensity = 0f
 
     private var mStatusBarHeight = 0
 
-    private var mProductType:String? = null
+    private var mProductType: String? = null
 
     private var mIsBiggerScreen = false
 
     @SuppressLint("InternalInsetResource", "DiscouragedApi")
     fun init(application: Application) {
         mContext = application
-        val displayManager:DisplayManager = application.getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
-        val defaultDisplay = displayManager.getDisplay(Display.DEFAULT_DISPLAY)
         val metrics = application.resources.displayMetrics
         mScreenWidthDp = max(metrics.heightPixels, metrics.widthPixels)
         mScreenHeightPx = max(metrics.heightPixels, metrics.widthPixels)
@@ -44,49 +44,104 @@ object AppManager {
         mScreenHeightDp = (mScreenHeightPx / mDensity).toInt()
 
 
-        val resourceId: Int = application.resources.getIdentifier("status_bar_height", "dimen", "android")
+        val resourceId: Int =
+            application.resources.getIdentifier("status_bar_height", "dimen", "android")
         mStatusBarHeight = application.resources.getDimensionPixelSize(resourceId)
     }
 
-    fun getScreenWidthPx():Int {
+    fun getScreenWidthPx(): Int {
         return mScreenWidthPx
     }
 
-    fun getScreenHeightPx():Int {
+    fun getScreenHeightPx(): Int {
         return mScreenHeightPx
     }
 
-    fun getScreenContentHeightPx():Int {
-        return  mScreenHeightPx - getStatusBarHeight()
+    fun getScreenContentHeightPx(): Int {
+        return mScreenHeightPx - getStatusBarHeight()
     }
 
-    fun getScreenWidthDp():Int {
+    fun getScreenWidthDp(): Int {
         return mScreenWidthDp
     }
 
-    fun getScreenHeightDp():Int {
+    fun getScreenHeightDp(): Int {
         return mScreenHeightDp
     }
 
-    fun getDensityDpi():Int {
+    fun getDensityDpi(): Int {
         return mDensityDpi
     }
 
-    fun getDensity():Float {
+    fun getDensity(): Float {
         return mDensity
     }
 
-    fun getStatusBarHeight():Int {
+    private fun getStatusBarHeight(): Int {
         return mStatusBarHeight
     }
 
 
-    fun getProductType():String? {
+    fun getProductType(): String? {
         return mProductType
     }
 
-    fun fun genProductType():String {
-        val model = DeviceIn
+    fun genProductType(): String {
+        val model = DeviceInfoUtils.phoneModel
+        return model.replace("[:{} \\[\\]\"']*", "")
+    }
+
+    fun getSmartBarHeight(): Int {
+        if (isMeizu() && hasSmartBar()) {
+            val autoHideSmartBar =
+                Settings.System.getInt(mContext.contentResolver, "mz_smartbar_auto_hide", 0) == 1
+            return if (autoHideSmartBar) {
+                0
+            } else {
+                getNormalNavigationBarHeight()
+            }
+        }
+        return 0
+    }
+
+    @SuppressLint("InternalInsetResource")
+    private fun getNormalNavigationBarHeight(): Int {
+        try {
+            val res: Resources = mContext.resources
+            val rid = res.getIdentifier("config_showNavigationBar", "bool", "android")
+            if (rid > 0) {
+                val flag = res.getBoolean(rid)
+                if (flag) {
+                    val resourceID = res.getIdentifier("navigation_bar_height", "dimen", "android")
+                    if (resourceID > 0) {
+                        return res.getDimensionPixelSize(resourceID)
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            LogUtil.e("getNormalNavigationBarHeight error = ${e.stackTrace}", throwable = e, tag = tag)
+        }
+        return 0
+    }
+
+    private fun isMeizu(): Boolean {
+        return DeviceInfoUtils.phoneManufacturer.equals("Meizu", ignoreCase = true)
+    }
+
+    private fun hasSmartBar(): Boolean {
+        try {
+            val method = Class.forName("android.os.Build").getMethod("hasSmartBar")
+            return method.invoke(null) as Boolean
+        } catch (e: Exception) {
+            LogUtil.e("has smart bar error = ${e.stackTrace}", throwable = e, tag = tag)
+        }
+
+        if (DeviceInfoUtils.phoneDevice == "mx2") {
+            return true
+        } else if (DeviceInfoUtils.phoneDevice == "mx" || DeviceInfoUtils.phoneDevice == "m9") {
+            return false
+        }
+        return false
     }
 
 }
--- SumTea_AndroidX/lib_framework/src/main/java/com/sistalk/framework/utils/DeviceInfoUtils.kt
+++ SumTea_AndroidX/lib_framework/src/main/java/com/sistalk/framework/utils/DeviceInfoUtils.kt
@@ -1,6 +1,16 @@
 package com.sistalk.framework.utils
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageManager
+import android.net.wifi.WifiManager
+import android.os.Build
+import android.provider.Settings
+import android.telephony.TelephonyManager
+import android.util.Log
+import com.sistalk.framework.log.LogUtil
+import java.io.FileInputStream
+import java.net.NetworkInterface
 
 /**
  * 设备信息的单例工具类
@@ -36,5 +46,140 @@ object DeviceInfoUtils {
         private set
     var phoneDevice:String = ""
         private set
+
+    fun init(context: Context) {
+        appContext = context.applicationContext
+        initDeviceInfo()
+    }
+
+    private fun initDeviceInfo() {
+        initImei()
+        initMac()
+        initAndroidID()
+        phoneModel = Build.MODEL
+        phoneBrand = Build.BRAND
+        phoneManufacturer = Build.MANUFACTURER
+        phoneDevice = Build.DEVICE
+    }
+
+    @SuppressLint("HardwareIds")
+    private fun initAndroidID() {
+        if (androidID.isNotEmpty()) return
+        LogUtil.d("start init device android id")
+        val tmpAndroidID = Settings.Secure.getString(appContext.contentResolver,Settings.Secure.ANDROID_ID,)
+        if (tmpAndroidID.isNullOrEmpty()) {
+            androidID = tmpAndroidID.lowercase()
+        }
+    }
+
+    private fun initMac() {
+        if (mac.isNotEmpty() || wifiMacAddress.isNotEmpty() || wifiSSID.isNotEmpty()) return
+
+        try {
+            LogUtil.d("start init device mac, wifi mac and ssid")
+            val vm = appContext.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+            initMacAddress(vm)
+            if (!vm.connectionInfo.bssid.isNullOrEmpty()) {
+                wifiMacAddress = vm.connectionInfo.bssid
+            }
+
+            if (!vm.connectionInfo.ssid.isNullOrEmpty()) {
+                wifiSSID = vm.connectionInfo.ssid
+            }
+        } catch (e:Exception) {
+            LogUtil.e("init mac error = ${e.stackTrace}", throwable = e)
+        }
+    }
+
+    private fun initMacAddress(vm:WifiManager) {
+        try {
+            // 6.0~7.0读取设备文件获取
+            val arrStrings = arrayOf("/sys/class/net/wlan0/address","/sys/devices/virtual/net/wlan0/address")
+            for (str in arrStrings) {
+                mac = readFile(str)
+                break
+            }
+            if (mac.isNotEmpty()) {
+                return
+            }
+
+            // 7.0以上获取
+            val interfaces = NetworkInterface.getNetworkInterfaces()?:return
+            while (interfaces.hasMoreElements()) {
+                val netInfo = interfaces.nextElement()
+                if ("wlan0" == netInfo.name) {
+                    val addresses = netInfo.hardwareAddress
+                    if (addresses == null || addresses.isEmpty()) {
+                        continue
+                    }
+                    mac = macByte2String(addresses)
+                    break
+                }
+            }
+        } catch (e:Exception) {
+            LogUtil.e("read mac error = ${e.stackTrace}", throwable = e)
+        }
+    }
+
+    @SuppressLint("HardwareIds")
+    private fun initImei() {
+        if (imei.isNotEmpty()|| imsi.isNotEmpty()) return
+        if (appContext.packageManager.checkPermission(android.Manifest.permission.READ_PHONE_STATE,
+                appContext.packageName) != PackageManager.PERMISSION_GRANTED) return
+        try {
+            LogUtil.d("start init device imei and imsi")
+            val tm = appContext.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+            var tmpImei = ""
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                tmpImei = tm.imei
+            } else {
+                tmpImei = tm.deviceId
+            }
+            if (tmpImei.isNotEmpty()) {
+                imei = tmpImei.lowercase()
+            }
+
+            val  tmpImsi = tm.subscriberId
+            if (!tmpImsi.isNullOrEmpty()) {
+                imsi = tmpImsi
+            }
+        } catch (e:Exception) {
+            LogUtil.e("init imei and imsi error = ${e.stackTrace}", throwable = e)
+        }
+    }
+
+    private fun macByte2String(bytes:ByteArray):String {
+        val buf = StringBuffer()
+        for (b in bytes) {
+            buf.append(String.format("%02X:",b))
+        }
+        if (buf.isNotEmpty()) {
+            buf.deleteCharAt(buf.length - 1)
+        }
+        return buf.toString()
+    }
+
+    private fun readFile(filePath:String):String {
+        var res = ""
+        var fin:FileInputStream? = null
+        try {
+            fin = FileInputStream(filePath)
+            val length = fin.available()
+            val buffer = ByteArray(length)
+            val count = fin.read(buffer)
+            if (count > 0) {
+                res = String(buffer,Charsets.UTF_8)
+            }
+        } catch (e:Exception) {
+            LogUtil.e("read file error = ${e.stackTrace}", throwable = e)
+        } finally {
+            try {
+                fin?.close()
+            } catch (e:Exception) {
+                LogUtil.e("close FileInputStream error = ${e.stackTrace}", throwable = e)
+            }
+        }
+        return res
+    }
 
 }
