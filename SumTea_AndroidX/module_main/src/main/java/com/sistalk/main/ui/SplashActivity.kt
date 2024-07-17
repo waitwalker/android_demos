4808f91edef2174af31af3b5a3d3c8b6c96b1425
waitwalker <waitwalker@163.com>
2024-07-17 08:49:50 +0800

    倒计时添加

--- SumTea_AndroidX/module_main/src/main/java/com/sistalk/main/ui/SplashActivity.kt
+++ SumTea_AndroidX/module_main/src/main/java/com/sistalk/main/ui/SplashActivity.kt
@@ -2,22 +2,20 @@ package com.sistalk.main.ui
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.sistalk.main.R
+import com.sistalk.framework.base.BaseDataBindActivity
+import com.sistalk.framework.ext.onClick
+import com.sistalk.framework.utils.StatusBarSettingHelper
+import com.sistalk.main.databinding.ActivitySplashBinding
 
 @SuppressLint("CustomSplashScreen")
-class SplashActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.splash_frame)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+class SplashActivity : BaseDataBindActivity<ActivitySplashBinding>() {
+
+    override fun initView(savedInstanceState: Bundle?) {
+        StatusBarSettingHelper.setStatusBarTranslucent(this)
+        mBinding.tvSkip.onClick {
+
         }
+
     }
+
 }
