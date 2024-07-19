b31606f229bd66c9549de823ab305cecaa0ca164
waitwalker <waitwalker@163.com>
2024-07-19 09:33:31 +0800

    Splash 处理

--- SumTea_AndroidX/module_main/build.gradle.kts
+++ SumTea_AndroidX/module_main/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(project(":lib_common"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- SumTea_AndroidX/module_main/src/main/java/com/sistalk/main/ui/SplashActivity.kt
+++ SumTea_AndroidX/module_main/src/main/java/com/sistalk/main/ui/SplashActivity.kt
@@ -1,10 +1,13 @@
 package com.sistalk.main.ui
-
 import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.sistalk.common.provider.MainServiceProvider
 import com.sistalk.framework.base.BaseDataBindActivity
 import com.sistalk.framework.ext.onClick
 import com.sistalk.framework.utils.StatusBarSettingHelper
+import com.sistalk.framework.utils.countDownCoroutines
+import com.sistalk.main.R
 import com.sistalk.main.databinding.ActivitySplashBinding
 
 @SuppressLint("CustomSplashScreen")
@@ -13,9 +16,15 @@ class SplashActivity : BaseDataBindActivity<ActivitySplashBinding>() {
     override fun initView(savedInstanceState: Bundle?) {
         StatusBarSettingHelper.setStatusBarTranslucent(this)
         mBinding.tvSkip.onClick {
-
+            MainServiceProvider.toMain(this)
         }
 
+        // 倒计时
+        countDownCoroutines(2,lifecycleScope, onTick = {
+            mBinding.tvSkip.text = getString(R.string.splash_time, it.plus(1).toString())
+        }) {
+            MainServiceProvider.toMain(this)
+            finish()
+        }
     }
-
 }
