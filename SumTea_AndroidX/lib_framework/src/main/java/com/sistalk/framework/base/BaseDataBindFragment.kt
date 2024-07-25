df5d9cb1d963ada8413a9caeccf730228c5add0f
waitwalker <waitwalker@163.com>
2024-07-25 09:19:39 +0800

    添加Fragment基类

---
+++ SumTea_AndroidX/lib_framework/src/main/java/com/sistalk/framework/base/BaseDataBindFragment.kt
@@ -0,0 +1,35 @@
+package com.sistalk.framework.base
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+import com.sistalk.framework.ext.saveAs
+import com.sistalk.framework.ext.saveAsUnChecked
+import java.lang.reflect.ParameterizedType
+
+/**
+ * databinding Fragment 基类
+ * */
+abstract class BaseDataBindFragment<DB:ViewBinding>:BaseFragment() {
+
+    var mBinding:DB? = null
+
+    override fun getContentView(inflater: LayoutInflater, container: ViewGroup?): View {
+        val type = javaClass.genericSuperclass
+        val vbClass:Class<DB> = type!!.saveAs<ParameterizedType>().actualTypeArguments[0].saveAs()
+        val method = vbClass.getDeclaredMethod("inflate",LayoutInflater::class.java)
+        mBinding = method.invoke(this,layoutInflater)!!.saveAsUnChecked()
+        return mBinding!!.root
+    }
+
+    override fun getLayoutResId(): Int {
+        return 0
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mBinding = null
+    }
+
+
+}
---
+++ SumTea_AndroidX/lib_framework/src/main/java/com/sistalk/framework/base/BaseMVVMFragment.kt
@@ -0,0 +1,25 @@
+package com.sistalk.framework.base
+
+import android.os.Bundle
+import android.view.View
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import java.lang.reflect.ParameterizedType
+
+/**
+ * ViewModel和Fragment进行绑定
+ * */
+abstract class BaseMVVMFragment<DB:ViewDataBinding,VM:ViewModel>:BaseDataBindFragment<DB>() {
+    lateinit var mViewModel: VM
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        initViewModel()
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    open fun initViewModel() {
+        val argument = (this.javaClass.genericSuperclass as ParameterizedType).actualTypeArguments
+        mViewModel = ViewModelProvider(this)[argument[1] as Class<VM>]
+    }
+}
