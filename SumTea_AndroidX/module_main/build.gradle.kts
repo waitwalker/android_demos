b0a0c9b58a2ef8e40529eede56153f3542f35b13
waitwalker <waitwalker@163.com>
2024-08-10 16:58:20 +0800

    首页布局添加

--- SumTea_AndroidX/module_main/build.gradle.kts
+++ SumTea_AndroidX/module_main/build.gradle.kts
@@ -71,6 +71,11 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(project(":network"))
+    implementation(libs.refresh.footer.classics)
+    implementation(libs.refresh.header.classics)
+    implementation(libs.refresh.layout.kernel)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
