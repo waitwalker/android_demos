84dbf59cbdb2497fb79a26a55c6000e4fceea881
waitwalker <waitwalker@163.com>
2024-08-19 09:28:45 +0800

    导入依赖

--- SumTea_AndroidX/lib_common/src/main/java/com/sistalk/common/utils/dpToPx.kt
+++ SumTea_AndroidX/lib_common/src/main/java/com/sistalk/common/utils/dpToPx.kt
@@ -1,7 +1,7 @@
 package com.sistalk.common.utils
 
 import android.util.TypedValue
-import com.sistalk.framework.helper
+import com.sistalk.framework.log
 
 /// dp 转 像素
 fun dpToPx(dpValue:Float):Float {
