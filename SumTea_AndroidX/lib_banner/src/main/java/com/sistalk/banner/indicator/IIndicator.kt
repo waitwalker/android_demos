a4a1033d92e0cd9f6a1699495eaa9899b353afb1
waitwalker <waitwalker@163.com>
2024-09-02 09:09:39 +0800

    Indicator接口完善

--- SumTea_AndroidX/lib_banner/src/main/java/com/sistalk/banner/indicator/IIndicator.kt
+++ SumTea_AndroidX/lib_banner/src/main/java/com/sistalk/banner/indicator/IIndicator.kt
@@ -1,8 +1,9 @@
 package com.sistalk.banner.indicator
 
 import androidx.viewpager.widget.ViewPager
+import com.sistalk.banner.options.IndicatorOptions
 
 interface IIndicator:ViewPager.OnPageChangeListener {
     fun notifyDataChanged()
-    fun setIndicatorOptions(options:)
+    fun setIndicatorOptions(options:IndicatorOptions)
 }
