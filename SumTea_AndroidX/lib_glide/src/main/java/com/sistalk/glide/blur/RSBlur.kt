a797e326606f4b9803f59fecb48cd1f5afd91e6f
waitwalker <waitwalker@163.com>
2024-09-12 09:13:06 +0800

    rs 模糊添加

--- SumTea_AndroidX/lib_glide/src/main/java/com/sistalk/glide/blur/RSBlur.kt
+++ SumTea_AndroidX/lib_glide/src/main/java/com/sistalk/glide/blur/RSBlur.kt
@@ -2,12 +2,43 @@ package com.sistalk.glide.blur
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.os.Build
+import android.renderscript.Allocation
+import android.renderscript.Element
 import android.renderscript.RSRuntimeException
+import android.renderscript.RenderScript
+import android.renderscript.RenderScript.RSMessageHandler
+import android.renderscript.ScriptIntrinsicBlur
 import kotlin.jvm.Throws
 
 object RSBlur {
     @Throws(RSRuntimeException::class)
-    fun blur(context: Context?, bitmap: Bitmap, radius:Int):Bitmap {
-
+    fun blur(context: Context?, bitmap: Bitmap, radius: Int): Bitmap {
+        var rs: RenderScript? = null
+        var input: Allocation? = null
+        var output: Allocation? = null
+        var blur: ScriptIntrinsicBlur? = null
+        try {
+            rs = RenderScript.create(context)
+            rs.messageHandler = RSMessageHandler()
+            input = Allocation.createFromBitmap(
+                rs,
+                bitmap,
+                Allocation.MipmapControl.MIPMAP_NONE,
+                Allocation.USAGE_SCRIPT
+            )
+            output = Allocation.createTyped(rs,input.type)
+            blur = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs))
+            blur.setInput(input)
+            blur.setRadius(radius.toFloat())
+            blur.forEach(output)
+            output.copyTo(bitmap)
+        } finally {
+            rs?.destroy()
+            input?.destroy()
+            output?.destroy()
+            blur?.destroy()
+        }
+        return bitmap
     }
 }
