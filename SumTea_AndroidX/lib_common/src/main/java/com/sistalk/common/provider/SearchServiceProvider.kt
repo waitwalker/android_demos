8f7974bda386717bcf0433eda583a9462c9a9321
waitwalker <waitwalker@163.com>
2024-09-23 20:44:36 +0800

    格式化

--- SumTea_AndroidX/lib_common/src/main/java/com/sistalk/common/provider/SearchServiceProvider.kt
+++ SumTea_AndroidX/lib_common/src/main/java/com/sistalk/common/provider/SearchServiceProvider.kt
@@ -4,12 +4,13 @@ import android.content.Context
 import com.alibaba.android.arouter.facade.annotation.Autowired
 import com.alibaba.android.arouter.launcher.ARouter
 import com.sistalk.common.service.ISearchService
+
 /**
  * 搜索服务类，对话提供相应的能力
  * */
 object SearchServiceProvider {
     @Autowired(name = "/search/service/search22")
-    lateinit var searchService:ISearchService
+    lateinit var searchService: ISearchService
 
     init {
         ARouter.getInstance().inject(this)
