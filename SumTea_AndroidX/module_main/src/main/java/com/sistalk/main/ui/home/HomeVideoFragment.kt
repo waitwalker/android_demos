e1e02189010eb6c48ab5e2025c8012a92adba904
waitwalker <waitwalker@163.com>
2024-10-14 21:37:55 +0800

    添加lib room

--- SumTea_AndroidX/module_main/src/main/java/com/sistalk/main/ui/home/HomeVideoFragment.kt
+++ SumTea_AndroidX/module_main/src/main/java/com/sistalk/main/ui/home/HomeVideoFragment.kt
@@ -8,11 +8,12 @@ import android.view.ViewGroup
 import com.sistalk.framework.base.BaseMVVMFragment
 import com.sistalk.main.R
 import com.sistalk.main.databinding.FragmentHomeVideoBinding
+import com.sistalk.main.ui.home.adapter.HomeVideoItemAdapter
 import com.sistalk.main.ui.home.viewmodel.HomeViewModel
 
 class HomeVideoFragment : BaseMVVMFragment<FragmentHomeVideoBinding,HomeViewModel>() {
 
-    private lateinit var videoAdapter:HomeVideoItemAdapter
+    private lateinit var videoAdapter: HomeVideoItemAdapter
 
     override fun initView(view: View, savedInstanceState: Bundle?) {
         TODO("Not yet implemented")
--- SumTea_AndroidX/module_main/src/main/java/com/sistalk/main/ui/home/adapter/HomeVideoItemAdapter.kt
+++ SumTea_AndroidX/module_main/src/main/java/com/sistalk/main/ui/home/adapter/HomeVideoItemAdapter.kt
@@ -1,6 +1,8 @@
 package com.sistalk.main.ui.home.adapter
 
 import android.content.Context
+import com.sistalk.framework.adapter.BaseRecycleViewAdapter
 
-class HomeVideoItemAdapter(val context: Context):BaseRe {
+class HomeVideoItemAdapter(val context: Context):
+    BaseRecycleViewAdapter<HomeVideoItem, ItemHomeVideoBinding>() {
 }
