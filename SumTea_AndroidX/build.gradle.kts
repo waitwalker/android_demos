87c049aed0d8eb65e35a1f65206255172f8f8023
waitwalker <waitwalker@163.com>
2024-10-28 09:23:23 +0800

    添加parcelize插件(添加插件的引用要在项目gradle中先引用)

--- SumTea_AndroidX/build.gradle.kts
+++ SumTea_AndroidX/build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kapt) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
 }
