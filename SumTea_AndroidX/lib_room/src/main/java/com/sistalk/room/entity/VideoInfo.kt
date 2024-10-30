499455267e39d31dd58ab2fb22e39f905e30c67d
waitwalker <waitwalker@163.com>
2024-10-30 21:53:22 +0800

    数据库表创建处理

--- SumTea_AndroidX/lib_room/src/main/java/com/sistalk/room/entity/VideoInfo.kt
+++ SumTea_AndroidX/lib_room/src/main/java/com/sistalk/room/entity/VideoInfo.kt
@@ -1,8 +1,36 @@
 package com.sistalk.room.entity
 
+import android.os.Parcelable
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 @Entity(tableName = "table_video_list")
-data class VideoInfo {
+data class VideoInfo(
+    // 主键
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0,
+
+    @ColumnInfo(name = "title", defaultValue = "")
+    var title: String?,
+
+    @ColumnInfo(name = "desc")
+    var desc: String?,
+
+    @ColumnInfo(name = "authorName")
+    var authorName:String?,
+
+    @ColumnInfo(name = "playUrl")
+    var playUrl: String?,
+
+    @ColumnInfo(name = "imageUrl")
+    var imageUrl: String?,
+
+    @Ignore
+    var collectionCount:String?
+) : Parcelable {
+
 }
