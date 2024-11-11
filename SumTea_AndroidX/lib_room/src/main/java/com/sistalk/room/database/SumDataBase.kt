01a950322b7924daa2a35d00ea468291ba364fbf
waitwalker <waitwalker@163.com>
2024-11-11 09:34:26 +0800

    添加注释

--- SumTea_AndroidX/lib_room/src/main/java/com/sistalk/room/database/SumDataBase.kt
+++ SumTea_AndroidX/lib_room/src/main/java/com/sistalk/room/database/SumDataBase.kt
@@ -3,6 +3,7 @@ package com.sistalk.room.database
 import androidx.room.Database
 import com.sistalk.room.entity.VideoInfo
 
+/// 数据库
 @Database(entities = [VideoInfo::class], version = 1, exportSchema = false)
 abstract class SumDataBase {
     abstract fun videoListDao()
